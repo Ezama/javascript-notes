b606a1118472fe81e44bb6c6ec72ee001dc6b1b5
Ezama <ezama.eazye@gmail.com>
2022-05-07 14:19:26 +0300

    async / await / fetch

--- async js/promises.js
+++ async js/promises.js
@@ -36,9 +36,18 @@ function createPost(post) {
     });
 }
 
-createPost({
-        title: "Post Three",
-        body: "This is post three"
-    }).then(getPosts)
-    .catch(err => console.log(err));
+// createPost({
+//         title: "Post Three",
+//         body: "This is post three"
+//     }).then(getPosts)
+//     .catch(err => console.log(err));
 
+//promise.all
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, "Goodbye")
+});
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json());
+
+Promise.all([promise1, promise2, promise3]).then(values => console.log(values));
